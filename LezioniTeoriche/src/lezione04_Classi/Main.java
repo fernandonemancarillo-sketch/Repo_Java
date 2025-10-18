868b71512287338a07c3cba5c72624095195df7e
fernandonemancarillo-sketch <fernando.neman.carillo@edu.immaginazioneelavoro.it>
2025-10-18 22:32:12 +0200

    Update Main.java

--- LezioniTeoriche/src/lezione04_Classi/Main.java
+++ LezioniTeoriche/src/lezione04_Classi/Main.java
@@ -3,12 +3,12 @@ package lezione04_Classi;
 public class Main {
 	public static void main(String[] args) {
 		Cambio teste = new Cambio("Nando",26,150);
-		Cambio lily = new Cambio ("Lily",23,1000);
-		
-		lily.UI();
-		lily.convertEuro();
+
+		teste.UI();
+	
 	
 		
 	}
 
 }
+
