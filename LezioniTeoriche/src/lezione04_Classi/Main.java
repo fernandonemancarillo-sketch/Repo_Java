bac32433b627ee2183f57f9c6169e2e37befd9c0
fernandonemancarillo-sketch <fernando.neman.carillo@edu.immaginazioneelavoro.it>
2025-10-18 21:19:24 +0200

    Add files via upload

---
+++ LezioniTeoriche/src/lezione04_Classi/Main.java
@@ -0,0 +1,14 @@
+package lezione04_Classi;
+
+public class Main {
+	public static void main(String[] args) {
+		Cambio teste = new Cambio("Nando",26,150);
+		Cambio lily = new Cambio ("Lily",23,1000);
+		
+		lily.UI();
+		lily.convertEuro();
+	
+		
+	}
+
+}
